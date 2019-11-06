deea0bee9d4d00b4e348ad2fac9c9f4790029ee7
Schuyler Conover <schuyler.conover@opentempo.com>
2019-11-06 10:15:16 -0500

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,10 +15,14 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? <QuizEnd /> :
-          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
         }
       </div>
   )}
+
+  showNextQuestion() {
+    this.setState({ quiz_position: this.state.quiz_position + 1 })
+  }
 }
 
 export default Quiz
